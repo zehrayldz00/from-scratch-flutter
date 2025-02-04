951de16e7a17599d65676b869e39d84949c98892
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-02-04 23:54:30 +0300

    SOLID Princibles & OOP & Page View 7

---
+++ lib/views/page_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageViewZGY extends StatefulWidget {
+  const PageViewZGY({super.key});
+
+  @override
+  State<PageViewZGY> createState() => _PageViewZGYState();
+}
+
+class _PageViewZGYState extends State<PageViewZGY> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
