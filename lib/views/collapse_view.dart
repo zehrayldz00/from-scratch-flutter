24cd405b347ea14c83d8516419cf8cfc9165a507
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-02-07 23:26:03 +0300

    Expansion Tile & Listview & HTML Parse

---
+++ lib/views/collapse_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CollapseView extends StatefulWidget {
+  const CollapseView({super.key});
+
+  @override
+  State<CollapseView> createState() => _CollapseViewState();
+}
+
+class _CollapseViewState extends State<CollapseView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
