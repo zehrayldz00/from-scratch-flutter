926f4a26927e49f484a1033d9859da10b2658ab7
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-02-09 00:52:33 +0300

    File Manager & Logging (Download , Read , Save , Write , Open)

---
+++ lib/views/file_download_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FileDownloadView extends StatefulWidget {
+  const FileDownloadView({super.key});
+
+  @override
+  State<FileDownloadView> createState() => _FileDownloadViewState();
+}
+
+class _FileDownloadViewState extends State<FileDownloadView> {
+
+  //3.16
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
