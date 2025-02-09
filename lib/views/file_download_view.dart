7e03b06e3df45ef57ff324a7b38b6bdd874da86e
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-02-09 23:49:18 +0300

    File Manager & Logging (Download , Read , Save , Write , Open) finished.

--- lib/views/file_download_view.dart
+++ lib/views/file_download_view.dart
@@ -21,17 +21,31 @@ class _FileDownloadViewState extends State<FileDownloadView> {
     return _devicePath.path;
   }
 
-  Future <File> get _localFile async{
+  Future <File>  _localFile({required String path, required String type}) async{
     String _path = await _localDevicePath;
-    var _newPath = await Directory("$_path/veli").create(recursive: true); // burada recursive:true olmadan ara dizinler oluşturulamaz.
-    return File("${_newPath.path}/selam.pdf");
+    var _newPath = await Directory("$_path/$path").create(recursive: true); // burada recursive:true olmadan ara dizinler oluşturulamaz.
+    return File("${_newPath.path}/selam.$type");
   }
 
 
-  Future _downloadSamplePDF() async{
+  /*Future _downloadSamplePDF() async{
     final _response = await http.get(Uri.parse('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'));
     if(_response.statusCode == 200){
-      final _saveFile = await _localFile;
+      final _saveFile = await _localFile(path :"veli", type: "pdf" );
+      await _saveFile.writeAsBytes(_response.bodyBytes); // yanıt başarılı ise pdfin byte verileri alınır. ve pdf cihaza kaydedilir.
+      Logger().i("File write completed. File Path ${_saveFile.path}");
+      setState(() {
+        _filePath = _saveFile.path;
+      });
+    }else{
+      Logger().e(_response.statusCode);
+    }
+  }*/
+
+  Future _downloadSampleVideo() async{
+    final _response = await http.get(Uri.parse('https://www.sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4'));
+    if(_response.statusCode == 200){
+      final _saveFile = await _localFile(path: "videos", type: "mp4");
       await _saveFile.writeAsBytes(_response.bodyBytes); // yanıt başarılı ise pdfin byte verileri alınır. ve pdf cihaza kaydedilir.
       Logger().i("File write completed. File Path ${_saveFile.path}");
       setState(() {
@@ -51,13 +65,13 @@ class _FileDownloadViewState extends State<FileDownloadView> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton(
-                onPressed: _downloadSamplePDF,
+                onPressed: _downloadSampleVideo,
                 icon: Icon(Icons.file_download),
             ),
             Text(_filePath),
             IconButton(
               onPressed: (){
-                OpenFile.open(_filePath, type:"text/plain");
+                OpenFile.open(_filePath);
               },
               icon: Icon(Icons.file_open),
             ),
