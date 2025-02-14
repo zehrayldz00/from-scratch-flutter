72528bca760f9e5d589d50be3b69ae1971536b98
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-02-14 23:52:22 +0300

    Hero

---
+++ lib/views/hero/hero_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HeroView extends StatefulWidget {
+  const HeroView({super.key});
+
+  @override
+  State<HeroView> createState() => _HeroViewState();
+}
+
+class _HeroViewState extends State<HeroView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
